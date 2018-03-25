b8d7fb0026bf600290a79ddb3ed693b4444143ff
alasmaria <alasmari_0007@hotmail.com>
2018-03-25 16:57:03 -0400

    fixed lab2run and lab2test, created static folder

--- labs/week2/lab2.py
+++ labs/week2/lab2.py
@@ -1,109 +1,81 @@
 """
+Lab 2
 lab2.py
-Abdullah
+Abdullah Alasmari
 1/30/2018
 """
-def squared(num_list):
+
+def squared_nums(num_list):
     """
     Squares numbers in num_list
-
     num_list: list of numbers
-
     Returns: list of these numbers squared
     """
-    new_list = [] #initialize list to hold results
-    for num in num_list: #iterate through num_list and square each element
-        sq_num = pow(num, 2) #square the numbers
-        new_list.append(sq_num)
-    return new_list
+    return_list = []
+    for number in num_list:
+        return_list.append(number ** 2)
 
-t1 = [1, 2, 3]
-t2
-res = squared(t1)
-
-    #2 **Function Interface:
+    return return_list
 
 def check_title(title_list):
-
     """
-
     Removes strings in title_list that have numbers and aren't title case
-
     title_list: list of strings
-
     Returns: list of strings that are titles
-
     """
-
-def check_title(title_list):
-    new_list = []
+    return_list = []
+    numbers = "0123456789"
     for title in title_list:
-        if title.istitle():
-            new_list.append(title)
-    return new_list
-
+        remove_title = False
+        for number in numbers:
+            if number in title:
+                remove_title = True
+        if title.istitle() == True and remove_title == False:
+            return_list.append(title)
+    return return_list
 
 def restock_inventory(inventory):
-
     """
-
     Increases inventory of each item in dictionary by 10
-
     inventory: a dictionary with:
-
-        key: string that is the name of the inventory item
-
-        value: integer that equals the number of that item currently on hand
-
+    ** key: string that is the name of the inventory item**
+    ** value: integer that equals the number of that item currently on hand**
     Returns: updated dictionary where each inventory item is restocked
-
     """
-    new_inventory = { }
-    for key, val in inventory.items( ):
-    	new_inventory[key] = val + 10
-    return new_inventory
-
+    return_dict = {}
+    for key, value in inventory.items():
+        return_dict[key] = inventory[key] + 10
+    return return_dict
 
 def filter_0_items(inventory):
-
     """
-
     Removes items that have a value of 0 from a dictionary of inventories
-
     inventory: dictionary with:
-
-       key: tring that is the name of the inventory item
-
-       value: nteger that equals the number of that item currently on hand
-
+    ** key: tring that is the name of the inventory item**
+    ** value: nteger that equals the number of that item currently on hand**
     Returns: the same inventory_dict with any item that had 0 quantity removed
-
     """
-    new_inventory = { }
-
-    for a, b in inventory.items ( ):
-        if v != 0:
-            new_inventory[k] = v
-    return new_inventory
-
+    return_dict = {}
+    for key, value in inventory.items():
+        if inventory[key] != 0:
+            return_dict[key] = inventory[key]
+    return return_dict
 
 def average_grades(grades):
-
     """
-
     Takes grade values from a dictionary and averages them into a final grade
-
     grades: a dictionary of grades with:
-
     key: string of students name
-
     value: list of integer grades received in class
-
     Returns: dictionary that averages out the grades of each student
-
     """
-    avgDict = {}
-    for student_name, student_grades in grades.items( ):
-        new_grades[student_name] = sum(student_grades) / len(student_grades)
-        # v is the list of grades for student k
-    return new_grades
+    return_dict = {}
+    for student, grades in grades.items():
+        these_grades = 0
+        num_grades = 0
+        for grade in grades:
+            num_grades += 1
+            these_grades += grade
+        final_grade = these_grades / num_grades
+        return_dict[student] = final_grade
+return return_dict
